ea31d75058b5c52eca4d63c44c099ed6b4569a29
muhammad-muchson <muchson@alterra.id>
2021-09-29 21:49:08 +0700

    menambahkan comit ku

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import './App.css';
-import {app} from './base'
+import React from "react";
+import "./App.css";
+import { app } from "./base";
 
 function App() {
   const onChange = (e) => {
     const file = e.target.files[0];
-    const storageRef = app.storage().ref()
-    const fileRef = storageRef.child(file.name)
-    fileRef.put(file).then(() => {
-      console.log("Uploaded a file")
-    })
-  }
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child(file.name);
+    console.log("file = ", file);
+    console.log("storageRef = ", storageRef);
+    console.log("fileRef = ", fileRef);
+    fileRef.put(file).then((e) => {
+      console.log("Uploaded a file");
+      console.log("didalam e = ", e);
+      e.ref.getDownloadURL().then(function (downloadURL) {
+        console.log("File available at", downloadURL);
+      });
+    });
+  };
 
-  return (
-    <input type="file" onChange={onChange}/>
-  );
+  return <input type="file" onChange={onChange} />;
 }
 
 export default App;
--- src/base.js
+++ src/base.js
@@ -1,14 +1,46 @@
 import * as firebase from "firebase/app";
 import "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBYxudpXLAB8sfKXsrW-3pL1AbxWJHcN-0",
+//   authDomain: "frb-fls-example.firebaseapp.com",
+//   databaseURL: "https://frb-fls-example.firebaseio.com",
+//   projectId: "frb-fls-example",
+//   storageBucket: "frb-fls-example.appspot.com",
+//   messagingSenderId: "971492637412",
+//   appId: "1:971492637412:web:7726a345b7189eacc74af1",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBYxudpXLAB8sfKXsrW-3pL1AbxWJHcN-0",
-  authDomain: "frb-fls-example.firebaseapp.com",
-  databaseURL: "https://frb-fls-example.firebaseio.com",
-  projectId: "frb-fls-example",
-  storageBucket: "frb-fls-example.appspot.com",
-  messagingSenderId: "971492637412",
-  appId: "1:971492637412:web:7726a345b7189eacc74af1",
+  apiKey: "AIzaSyCgldcIJ3QOlAniQ0zMmHEj7PbNCqMWBo0",
+  authDomain: "upload-file-306ad.firebaseapp.com",
+  projectId: "upload-file-306ad",
+  storageBucket: "upload-file-306ad.appspot.com",
+  messagingSenderId: "915413354747",
+  appId: "1:915413354747:web:81ef7fc9426fe1baebc85e",
+  measurementId: "G-GCTXD9X27N",
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
+
+// ===================================================================
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCgldcIJ3QOlAniQ0zMmHEj7PbNCqMWBo0",
+//   authDomain: "upload-file-306ad.firebaseapp.com",
+//   projectId: "upload-file-306ad",
+//   storageBucket: "upload-file-306ad.appspot.com",
+//   messagingSenderId: "915413354747",
+//   appId: "1:915413354747:web:81ef7fc9426fe1baebc85e",
+//   measurementId: "G-GCTXD9X27N",
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
