f1c6acffe2a61f088bb1cd9ba958bae6f8407ba7
attoyibi <muchson@alterra.id>
2021-12-21 15:44:11 +0700

    drop down component
    membuat component drop down sederhana

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Example from "./Example";
+import DropDown from "./Example";
 
 function App() {
   return (
     <div className="app">
-      <Example />
+      <DropDown />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>;
     </div>
   );
